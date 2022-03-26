b163d3421139905e7377244d3d881128dd712671
Zahra <bghzahra@outlook.com>
2022-03-26 11:46:17 +0000

    changed form to a div tag in form component

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,13 +1,13 @@
-export default function Form (): JSX.Element{
+export default function div (): JSX.Element{
 return(
     <main>
-        <form>
+        <div className="form">
             <input type="text" className="form-input"/>
             <input type="text" className="form-input"/>
             <button className="new-meme-button">
             Get a new meme image  🖼
             </button>
-        </form>
+        </div>
     </main>
     )
 }
